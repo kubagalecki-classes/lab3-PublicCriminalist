7903e643ec35c8b1a131c1930f87d139e4a81448
PublicCriminali <01123869@pw.edu.pl>
2020-11-25 10:32:56 +0000

    rozwiazanie

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,14 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int dostawa,magazyn=0,liczba=0;
+    Stocznia stocznia{};
+    while(towar>magazyn){
+      Statek* s1=stocznia();
+      dostawa=s1->transportuj();
+      magazyn+=dostawa;
+      if(dynamic_cast<Zaglowiec*>(s1)) liczba++;
+      delete s1;
+    }
+    return liczba;
 }
